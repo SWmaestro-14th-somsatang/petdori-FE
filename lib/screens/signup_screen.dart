b0d77378915e05818cdbf6df9090955ece5add05
Cho-SangHyun <98shcho@naver.com>
2023-08-10 19:31:31 +0900

    feat(#15): 회원가입 시 중성화 필드 추가

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -21,7 +21,8 @@ class SignupScreen extends StatefulWidget {
 ///
 /// [_dogTypes] 강아지 종류로 선택가능한 목록
 /// [_genderTypes] 강아지 성별로 선택가능한 목록
-/// [_userName], [_dogName], [_dogType], [_dogGender], [_dogAge] 유저가 입력하는 값
+/// [_neuteredTypes] 강아지 중성화 여부로 선택가능한 목록
+/// [_userName], [_dogName], [_dogType], [_dogGender], [_isNeutered], [_dogAge] 유저가 입력하는 값
 /// [_isReady] 모든 필드가 입력되었는지 여부
 class _SignupScreenState extends State<SignupScreen> {
   final storage = const FlutterSecureStorage();
@@ -36,8 +37,10 @@ class _SignupScreenState extends State<SignupScreen> {
     '요크셔테리어'
   ];
   final List<String> _genderTypes = ['수컷', '암컷'];
+  final List<String> _neuteredTypes = ['중성화함', '중성화하지 않음'];
 
   String? _userName, _dogName, _dogType, _dogGender;
+  bool? _isNeutered;
   int? _dogAge;
   bool _isReady = false;
 
@@ -47,6 +50,7 @@ class _SignupScreenState extends State<SignupScreen> {
         _dogName != null &&
         _dogType != null &&
         _dogGender != null &&
+        _isNeutered != null &&
         _dogAge != null;
   }
 
@@ -62,6 +66,7 @@ class _SignupScreenState extends State<SignupScreen> {
           "dog_name": _dogName,
           "dog_type": _dogType,
           "dog_gender": _dogGender,
+          "is_neutered": _isNeutered,
           "dog_age": _dogAge,
         },
       );
@@ -84,7 +89,7 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // 화면 높이
+    final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
 
     final arguments =
@@ -178,20 +183,46 @@ class _SignupScreenState extends State<SignupScreen> {
                       _isReady = false;
                     });
                   },
+                  width: screenWidth * 0.9,
                 ),
-                SignupDropDownField(
-                  label: "강아지 성별",
-                  items: _genderTypes,
-                  onChanged: (value) {
-                    setState(() {
-                      _dogGender = value;
-                      if (areAllFieldFilled()) {
-                        _isReady = true;
-                        return;
-                      }
-                      _isReady = false;
-                    });
-                  },
+                SizedBox(
+                  width: screenWidth * 0.9,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SignupDropDownField(
+                        label: "강아지 성별",
+                        items: _genderTypes,
+                        onChanged: (value) {
+                          setState(() {
+                            _dogGender = value;
+                            if (areAllFieldFilled()) {
+                              _isReady = true;
+                              return;
+                            }
+                            _isReady = false;
+                          });
+                        },
+                        width: screenWidth * 0.425,
+                      ),
+                      SignupDropDownField(
+                        label: "중성화 여부",
+                        items: _neuteredTypes,
+                        onChanged: (value) {
+                          setState(() {
+                            _isNeutered =
+                                value == _neuteredTypes[0] ? true : false;
+                            if (areAllFieldFilled()) {
+                              _isReady = true;
+                              return;
+                            }
+                            _isReady = false;
+                          });
+                        },
+                        width: screenWidth * 0.425,
+                      ),
+                    ],
+                  ),
                 ),
                 Container(
                   margin: const EdgeInsets.only(top: 40),
