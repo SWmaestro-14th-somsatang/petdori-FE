c6c5afc4a3f2384603b785bbb281f268d30092ad
Cho-SangHyun <98shcho@naver.com>
2023-10-20 16:58:03 +0900

    refactor: 백엔드 base url 지정

--- lib/services/auth_service/auth_service.dart
+++ lib/services/auth_service/auth_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:wooyoungsoo/utils/constants.dart';
 import 'package:wooyoungsoo/models/base_response_model.dart';
 import 'package:wooyoungsoo/services/auth_service/resource_server/apple_server.dart';
 import 'package:wooyoungsoo/services/auth_service/resource_server/google_server.dart';
@@ -34,7 +35,7 @@ class AuthService {
     Dio dio = Dio();
     try {
       var res = await dio.post(
-        "http://localhost:8080/api/auth/signup?provider=$oauth2Provider",
+        "$baseURL/api/auth/signup?provider=$oauth2Provider",
         data: data,
       );
 
@@ -147,7 +148,7 @@ class AuthService {
     try {
       Dio dio = Dio();
       var res = await dio.post(
-        "http://localhost:8080/api/auth/reissue",
+        "$baseURL/api/auth/reissue",
         options: Options(
           headers: {
             "Content-Type": "application/json",
