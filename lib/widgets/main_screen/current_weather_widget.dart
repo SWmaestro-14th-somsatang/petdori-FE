1028858e94158a3cc0a91ba86bacd8a1172e0f5e
Cho-SangHyun <98shcho@naver.com>
2023-10-28 12:16:23 +0900

    feat(main-page): 위치기반 행정구역 조회기능 추가

--- lib/widgets/main_screen/current_weather_widget.dart
+++ lib/widgets/main_screen/current_weather_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:wooyoungsoo/services/region_info_service/region_info_service.dart';
 import 'package:wooyoungsoo/utils/constants.dart';
+import 'package:geolocator/geolocator.dart';
 
 /// 메인 화면 현재 날씨
 ///
 /// [screenWidth] : 화면 너비
-class CurrentWeather extends StatelessWidget {
+class CurrentWeather extends StatefulWidget {
   const CurrentWeather({
     super.key,
     required this.screenWidth,
@@ -13,9 +16,62 @@ class CurrentWeather extends StatelessWidget {
   final double screenWidth;
 
   @override
+  State<CurrentWeather> createState() => _CurrentWeatherState();
+}
+
+/// 메인 화면 현재 날씨의 state
+///
+/// [currentPosition] 현재 위치
+/// [sigu] 현재 위치의 시-구 정보
+/// [isLoading] 현재 위치를 불러오는 중인지 여부
+class _CurrentWeatherState extends State<CurrentWeather> {
+  final RegionInfoService regionInfoService = RegionInfoService();
+  late Position currentPosition;
+  late String sigu;
+  bool isLoading = true;
+
+  Future _determineSigu() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    currentPosition = await Geolocator.getCurrentPosition();
+    sigu = await regionInfoService.getRegionInfo(
+      latitude: currentPosition.latitude,
+      longitude: currentPosition.longitude,
+    );
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _determineSigu();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      width: screenWidth * 0.934,
+      width: widget.screenWidth * 0.934,
       decoration: BoxDecoration(
         color: whiteColor,
         borderRadius: BorderRadius.circular(20),
@@ -32,66 +88,78 @@ class CurrentWeather extends StatelessWidget {
           Column(
             children: [
               const SizedBox(height: 25),
-              RichText(
-                text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "지금 ",
-                      style: TextStyle(
-                        color: blackColor,
-                        fontSize: 18,
-                        fontWeight: fontWeightRegular,
-                        fontFamily: "NotoSansKR",
-                      ),
-                    ),
-                    TextSpan(
-                      text: "서울시 강남구",
-                      style: TextStyle(
+              isLoading
+                  ? const SizedBox(
+                      height: 90,
+                      child: SpinKitCircle(
                         color: mainColor,
-                        fontSize: 18,
-                        fontWeight: fontWeightBold,
-                        fontFamily: "NotoSansKR",
+                        size: 50,
                       ),
+                    )
+                  : Column(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              const TextSpan(
+                                text: "지금 ",
+                                style: TextStyle(
+                                  color: blackColor,
+                                  fontSize: 18,
+                                  fontWeight: fontWeightRegular,
+                                  fontFamily: "NotoSansKR",
+                                ),
+                              ),
+                              TextSpan(
+                                text: sigu,
+                                style: const TextStyle(
+                                  color: mainColor,
+                                  fontSize: 18,
+                                  fontWeight: fontWeightBold,
+                                  fontFamily: "NotoSansKR",
+                                ),
+                              ),
+                              const TextSpan(
+                                text: "는...",
+                                style: TextStyle(
+                                  color: blackColor,
+                                  fontSize: 18,
+                                  fontWeight: fontWeightRegular,
+                                  fontFamily: "NotoSansKR",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        const Row(
+                          children: [
+                            Text(
+                              "⛅️",
+                              style: TextStyle(
+                                fontSize: 34,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "조금은 흐린 날씨",
+                              style: TextStyle(
+                                color: blackColor,
+                                fontSize: 18,
+                                fontWeight: fontWeightMedium,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                    TextSpan(
-                      text: "는...",
-                      style: TextStyle(
-                        color: blackColor,
-                        fontSize: 18,
-                        fontWeight: fontWeightRegular,
-                        fontFamily: "NotoSansKR",
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                children: [
-                  Text(
-                    "⛅️",
-                    style: TextStyle(
-                      fontSize: 34,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "조금은 흐린 날씨",
-                    style: TextStyle(
-                      color: blackColor,
-                      fontSize: 18,
-                      fontWeight: fontWeightMedium,
-                    ),
-                  )
-                ],
-              ),
               const SizedBox(height: 25),
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  fixedSize: Size(screenWidth * 0.868, 50),
+                  fixedSize: Size(widget.screenWidth * 0.868, 50),
                   backgroundColor: mainColor,
                   foregroundColor: whiteColor,
                   shadowColor: Colors.transparent,
